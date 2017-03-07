dd627afe1864bd6025638bc3680e0feec3433ebe
Jamen Marzonie <jamenmarz@gmail.com>
2017-03-07 05:49:13 -0600

    Updates

---
+++ index.bundle.js
@@ -0,0 +1,13 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports=[
+  "audiojs/audio",
+  "audiojs/audio-buffer",
+  "audiojs/audio-oscillator",
+  "audiojs/audio-speaker"
+]
+
+},{}],2:[function(require,module,exports){
+var modules = require('../modules.json')
+console.log(modules)
+
+},{"../modules.json":1}]},{},[2]);
